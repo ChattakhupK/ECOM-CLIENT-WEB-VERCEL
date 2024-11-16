804e813b6df210785936cac20dedca725a8624eb
Chattakhup Kinaubon <MosT5140@hotmail.com>
2024-11-16 21:19:02 +0700

    fix text components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import ContentCarousel from "../components/home/contentCarousel";
+import ContentCarousel from "../components/home/ContentCarousel";
 import BestSeller from "../components/home/BestSeller";
 import NewProduct from "../components/home/NewProduct";
-
 const Home = () => {
   return (
     <div>
